0183ce19cf921ced1b7ff43246a5966325dfc67f
Dave Bouwman <dave@davebouwman.com>
2022-05-10 08:24:29 -0600

    feat(src-sites): getMoreNames

--- packages/srh-sites/src/index.ts
+++ packages/srh-sites/src/index.ts
@@ -3,3 +3,9 @@ import { srhFetch } from 'srh-common';
 export function getNames(url: string): Promise<string[]> {
   return srhFetch(url);
 }
+
+export function getMoreNames(url: string): Promise<string[]> {
+  return Promise.all([srhFetch(url), srhFetch(url)]).then(([a, b]) => {
+    return [...a, ...b];
+  });
+}
