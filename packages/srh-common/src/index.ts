cdb4898733ee5c19a2ba87316764d8ab78a10c11
Dave Bouwman <dave@davebouwman.com>
2022-05-10 08:06:19 -0600

    fix(srh-common): add missing string

--- packages/srh-common/src/index.ts
+++ packages/srh-common/src/index.ts
@@ -1,4 +1,4 @@
 export function srhFetch(url: string): Promise<string[]> {
-  console.log(`srhCommon:srhFetch ${url}`);
+  console.log(`srhCommon:srhFetch ${url} woot`);
   return Promise.resolve(['Allen', 'Bob', 'Carl']);
 }
